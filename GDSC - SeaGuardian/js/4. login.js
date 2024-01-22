2f9e8a9f0f06bb583858e4c1cec164745b56e9a4
Rudra Pratap Singh <144825212+RudraSingh05@users.noreply.github.com>
2024-01-22 13:48:25 +0530

    Final website

---
+++ GDSC - SeaGuardian/js/4. login.js
@@ -0,0 +1,29 @@
+var animateButton = function (e) {
+
+    e.preventDefault;
+    e.target.classList.remove('animate');
+    e.target.classList.add('animate');
+    setTimeout(function () {
+      e.target.classList.remove('animate');
+    }, 700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
+  
+  
+  
+  function moveToNext(currentInput) {
+    const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+    const currentLength = currentInput.value.length;
+  
+    if (currentLength === maxLength) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+  }
